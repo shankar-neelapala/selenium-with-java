67fd5e697b3c7b99ed351cf04fc1577ed0d85a56
Shankar1728 <neelapalashankar876@gmail.com>
2026-02-03 07:56:07 +0530

    navigational methods

---
+++ seleniumwebdriver/src/test/java/day6/NavigationalMethods.java
@@ -0,0 +1,27 @@
+package day6;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationalMethods {
+	public static void main(String args[]) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		
+		driver.get("https://testautomationpractice.blogspot.com/");
+		//get() and navigate.to() both are same
+		driver.navigate().to("https://www.pavanonlinetrainings.com/p/udemy-courses.html");
+		
+		
+		driver.navigate().back();
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(3000);
+		
+		driver.navigate().refresh();//reload the page
+	}
+}
